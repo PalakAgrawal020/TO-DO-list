f62ab9de2d71dcf81e14d83c022239bb08d950d9
Palak <palakagrawal020@gmail.com>
2023-10-12 12:36:39 +0530

    Add the feature of Adding a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './style.css';
 
-const container = document.getElementById('container');
+const list = document.getElementById('list');
+
+const btn = document.getElementById('list-btn');
+btn.addEventListener('click', taskManager);
+
+
+function taskManager() {
+
+    const input = document.createElement('input');
+    input.id='task'
+    input.type = 'text';
+
+    const addBtn = document.createElement('button');
+    addBtn.textContent = 'Add';
+
+    const cancelBtn = document.createElement('button');
+    cancelBtn.textContent = 'Cancel';
+
+    list.appendChild(input);
+    list.appendChild(addBtn);
+    list.appendChild(cancelBtn)
+
+    addBtn.addEventListener('click', createTask);
+    cancelBtn.addEventListener('click', removeTask);
+
+    function createTask() {
+        const task = document.createElement('div');
+        task.innerText = input.value;
+        list.removeChild(input);
+        list.removeChild(addBtn);
+        list.removeChild(cancelBtn);
+        list.appendChild(task);
+    };
+
+    function removeTask() {
+        list.removeChild(input);
+        list.removeChild(addBtn);
+        list.removeChild(cancelBtn);
+    };
+}
+
+
+
+
