ab6ca3293bc2dd1c07c6028c2df86f964c38421c
Palak <palakagrawal020@gmail.com>
2023-10-12 19:39:16 +0530

    Add the functionality to the delete, checkbox after adding the task, add the date to specify the due date

--- src/index.js
+++ src/index.js
@@ -11,44 +11,72 @@ function taskManager() {
     const input = document.createElement('input');
     input.id='task'
     input.type = 'text';
+    input.classList.add('input-field');
+
+    const AddCancelDiv = document.createElement('div');
 
     const addBtn = document.createElement('button');
     addBtn.textContent = 'Add';
+    addBtn.classList.add('add-button');
 
     const cancelBtn = document.createElement('button');
     cancelBtn.textContent = 'Cancel';
+    cancelBtn.classList.add('cancel-button');
+
+    AddCancelDiv.appendChild(addBtn);
+    AddCancelDiv.appendChild(cancelBtn)
 
     list.appendChild(input);
-    list.appendChild(addBtn);
-    list.appendChild(cancelBtn)
+    list.appendChild(AddCancelDiv);
 
     addBtn.addEventListener('click', createTask);
     cancelBtn.addEventListener('click', removeTask);
 
     function createTask() {
         const task = document.createElement('div');
-    
+        task.classList.add('task-title');
+
+        const leftSideTask = document.createElement('div');
+        leftSideTask.innerText = input.value;
+
+        const rightSideTask = document.createElement('div');
         const check = document.createElement('input');
         check.type = 'checkbox';
-        task.appendChild(check);
-    
-        task.innerText = input.value;
+        check.addEventListener('change', function() {
+            if (check.checked) {
+                task.style.textDecoration = 'line-through';
+                task.style.color = 'lightgray';
+            } else {
+                task.style.textDecoration = 'none';
+                task.style.color = 'black';
+            }
+        });
+        rightSideTask.appendChild(check);
+        
+
+        const addDate = document.createElement('input');
+        addDate.type = 'date';
+        rightSideTask.appendChild(addDate);
     
         const deleteTask = document.createElement('button');
         deleteTask.textContent = "delete";
-        task.appendChild(deleteTask);
+        deleteTask.addEventListener('click', () => {
+            task.remove();
+        })
+        rightSideTask.appendChild(deleteTask);
+
+        task.appendChild(leftSideTask);
+        task.appendChild(rightSideTask);
     
         list.removeChild(input);
-        list.removeChild(addBtn);
-        list.removeChild(cancelBtn);
+        list.removeChild(AddCancelDiv);
         list.appendChild(task);
     }
     
 
     function removeTask() {
         list.removeChild(input);
-        list.removeChild(addBtn);
-        list.removeChild(cancelBtn);
+        list.removeChild(AddCancelDiv);
     };
 }
 
