78be07fa86a18db85b55afa460edc381bf493f4b
Palak <palakagrawal020@gmail.com>
2023-10-13 13:20:56 +0530

    Refactor the index.js file functions into classes, change the style of the webpage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
+
 import './style.css';
 
-const list = document.getElementById('list');
+class Task {
+  constructor(text, date) {
+    this.text = text;
+    this.date = date;
+    this.completed = false;
+  }
 
-const btn = document.getElementById('list-btn');
-btn.addEventListener('click', taskManager);
+  toggleCompletion() {
+    this.completed = !this.completed;
+  }
+}
 
+class TaskManager {
+  constructor() {
+    this.taskArr = [];
+    this.list = document.getElementById('list');
+    this.btn = document.getElementById('list-btn');
+    this.input = document.createElement('input');
+    this.input.id = 'task';
+    this.input.type = 'text';
+    this.input.classList.add('input-field');
 
-function taskManager() {
+    this.addCancelDiv = document.createElement('div');
+    this.createButtons();
 
-    const input = document.createElement('input');
-    input.id='task'
-    input.type = 'text';
-    input.classList.add('input-field');
+    this.btn.addEventListener('click', () => this.addTaskInput());
+  }
 
-    const AddCancelDiv = document.createElement('div');
-
+  createButtons() {
     const addBtn = document.createElement('button');
     addBtn.textContent = 'Add';
     addBtn.classList.add('add-button');
@@ -23,63 +38,97 @@ function taskManager() {
     cancelBtn.textContent = 'Cancel';
     cancelBtn.classList.add('cancel-button');
 
-    AddCancelDiv.appendChild(addBtn);
-    AddCancelDiv.appendChild(cancelBtn)
+    addBtn.addEventListener('click', () => this.createTask());
+    cancelBtn.addEventListener('click', () => this.removeTaskInput());
 
-    list.appendChild(input);
-    list.appendChild(AddCancelDiv);
+    this.addCancelDiv.appendChild(addBtn);
+    this.addCancelDiv.appendChild(cancelBtn);
+  }
 
-    addBtn.addEventListener('click', createTask);
-    cancelBtn.addEventListener('click', removeTask);
+  addTaskInput() {
+    this.list.appendChild(this.input);
+    this.list.appendChild(this.addCancelDiv);
+  }
 
-    function createTask() {
-        const task = document.createElement('div');
-        task.classList.add('task-title');
+  removeTaskInput() {
+    this.list.removeChild(this.input);
+    this.list.removeChild(this.addCancelDiv);
+  }
 
-        const leftSideTask = document.createElement('div');
-        leftSideTask.innerText = input.value;
+  createTask() {
+    const taskText = this.input.value.trim();
+    if (taskText) {
+      const addDate = document.createElement('input');
+      addDate.type = 'date';
 
-        const rightSideTask = document.createElement('div');
-        const check = document.createElement('input');
-        check.type = 'checkbox';
-        check.addEventListener('change', function() {
-            if (check.checked) {
-                task.style.textDecoration = 'line-through';
-                task.style.color = 'lightgray';
-            } else {
-                task.style.textDecoration = 'none';
-                task.style.color = 'black';
-            }
-        });
-        rightSideTask.appendChild(check);
-        
+      const task = new Task(taskText, addDate.value);
 
-        const addDate = document.createElement('input');
-        addDate.type = 'date';
-        rightSideTask.appendChild(addDate);
-    
-        const deleteTask = document.createElement('button');
-        deleteTask.textContent = "delete";
-        deleteTask.addEventListener('click', () => {
-            task.remove();
-        })
-        rightSideTask.appendChild(deleteTask);
+      this.taskArr.push(task);
 
-        task.appendChild(leftSideTask);
-        task.appendChild(rightSideTask);
-    
-        list.removeChild(input);
-        list.removeChild(AddCancelDiv);
-        list.appendChild(task);
+      const taskDiv = document.createElement('div');
+      taskDiv.classList.add('task-title');
+
+      // Left Side: Checkbox and Task Title
+      const leftSideTask = document.createElement('div');
+      leftSideTask.style.display = 'flex';
+      leftSideTask.style.alignItems = 'center';
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.style.marginRight = '6px';
+      checkbox.addEventListener('change', () => {
+        task.toggleCompletion();
+        this.updateTaskAppearance(task, taskDiv);
+      });
+
+      const taskTitle = document.createElement('div');
+      taskTitle.innerText = task.text;
+
+      leftSideTask.appendChild(checkbox);
+      leftSideTask.appendChild(taskTitle);
+
+      // Right Side: Date and Delete Button
+      const rightSideTask = document.createElement('div');
+      rightSideTask.style.display = 'flex';
+      rightSideTask.style.alignItems = 'center';
+
+      const deleteTask = document.createElement('button');
+      deleteTask.textContent = 'Delete';
+      deleteTask.style.marginRight = '6px';
+      deleteTask.addEventListener('click', () => this.deleteTask(task, taskDiv));
+
+      rightSideTask.appendChild(addDate);
+      rightSideTask.appendChild(deleteTask);
+
+      taskDiv.appendChild(leftSideTask);
+      taskDiv.appendChild(rightSideTask);
+
+      this.list.removeChild(this.input);
+      this.list.removeChild(this.addCancelDiv);
+      this.updateTaskAppearance(task, taskDiv);
+
+      this.list.appendChild(taskDiv);
     }
-    
-
-    function removeTask() {
-        list.removeChild(input);
-        list.removeChild(AddCancelDiv);
-    };
 }
 
+  updateTaskAppearance(task, taskDiv) {
+    if (task.completed) {
+      taskDiv.style.textDecoration = 'line-through';
+      taskDiv.style.color = 'lightgray';
+    } else {
+      taskDiv.style.textDecoration = 'none';
+      taskDiv.style.color = 'black';
+    }
+  }
 
+  deleteTask(task, taskDiv) {
+    const index = this.taskArr.indexOf(task);
+    if (index !== -1) {
+      this.taskArr.splice(index, 1);
+    }
 
+    this.list.removeChild(taskDiv);
+  }
+}
 
+const taskManager = new TaskManager();
